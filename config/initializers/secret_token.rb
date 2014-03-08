89574ad1bca0447a3d9a35ea2dde4d582156a91e
Anne Maiale <amaiale@gmail.com>
2014-03-08 17:07:38 -0500

    first commit for devise version of mpts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseMPTS::Application.config.secret_key_base = '479e8c8f3a11ffd7df35d1c17329a64438a02b3143acfe2ef0ebeb4ccc8b57c9becea1264c90e37ce2d08708ca504f32a2f8b991009b728785f55b97abd00c28'
